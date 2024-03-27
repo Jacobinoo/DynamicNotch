a8021e6fe99396d9e58ac40689b48342614966f1
Jacob <69864916+Jacobinoo@users.noreply.github.com>
2024-03-27 14:29:20 +0100

    Initial Commit

---
+++ DynamicNotch/DynamicNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicNotchApp.swift
+//  DynamicNotch
+//
+//  Created by Jakub on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DynamicNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
