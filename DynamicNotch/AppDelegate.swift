475334851e62b86db570c7c74acda8d9b8f7e130
Jacob <69864916+Jacobinoo@users.noreply.github.com>
2024-04-02 18:41:55 +0200

    Initial commit

---
+++ DynamicNotch/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  DynamicNotch
+//
+//  Created by Jakub on 27/03/2024.
+//
+
+import Foundation
---
+++ DynamicNotch/Screen.swift
@@ -0,0 +1,8 @@
+//
+//  Screen.swift
+//  DynamicNotch
+//
+//  Created by Jakub on 27/03/2024.
+//
+
+import Foundation
